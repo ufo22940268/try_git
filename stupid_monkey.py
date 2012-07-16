40a55cb8f814996a67f47a3bb5f0b7edd33fdf7c
ufo22940268 <ufo22940268@gmail.com>
2012-07-16 09:22:05 +0800

    asdfa

---
+++ stupid_monkey.py
@@ -0,0 +1,13 @@
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+import time
+import datetime
+
+device = MonkeyRunner.waitForConnection()
+file  = open("monkey.log", "w")
+print "The log is writting to file: monkey.log. If you want to quit, please press CTRL+C."
+while True :
+    time.sleep(10)
+    file.write(str(datetime.datetime.now()) + "\n")
+    file.write(device.shell("ps"))
+    for _ in range(4):
+        file.write("\n")
